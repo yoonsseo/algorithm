6f44e226d7784d49596ba75c90c7c21c685f9873
yoonsseo <yellisa2000@gmail.com>
2024-07-27 04:50:28 +0900

    [Silver II]

---
+++ src/dp/boj18353_병사배치하기.java
@@ -0,0 +1,33 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj18353_병사배치하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+
+        int count = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            count = Math.max(count, dp[i]);
+        }
+
+        count = N - count;
+        System.out.println(count);
+    }
+}
