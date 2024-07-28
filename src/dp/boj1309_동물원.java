066f6863fc6b11461abbeafd3373ddeffce4ea0b
yoonsseo <yellisa2000@gmail.com>
2024-07-28 20:31:51 +0900

    [Silver 1]

--- src/dp/boj1309_동물원.java
+++ src/dp/boj1309_동물원.java
@@ -5,6 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class boj1309_동물원 {
+    /*
+    dp[i]를 구할 때
+
+    dp[i - 1]번째 줄에 사자가 없으면
+    i번째 줄에 사자가 없거나, 왼쪽 칸에 있거나 오른쪽 칸에 있거나 3가지 경우
+
+    dp[i - 1]번째 줄에 사자가 있으면
+    i번째 줄에 사자가 없거나, 사자가 i - 1 번째 줄에 있는 사자와 다른 칸에 있는 2가지 경우
+     */
     static int[] init = {0, 3, 7};
 
     public static void main(String[] args) throws IOException {
